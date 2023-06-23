98d4e9b11286f292d258f8fbb575cf32bbb59a02
sikku85 <sikku.sauravkbc@gmail.com>
2023-06-24 02:11:06 +0530

    ok

--- src/Components/Admin.js
+++ src/Components/Admin.js
@@ -1,6 +1,5 @@
 import React, { useState,useEffect } from 'react';
 import axios from 'axios';
-import { formatMs } from '@material-ui/core';
 import "./admin.css";
 // import { Spinner } from './Spinner';
 import { NewSpinner } from './NewSpinner';
